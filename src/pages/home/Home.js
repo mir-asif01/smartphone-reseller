6f1f36cdc2e881ce26ceffab7c8ba15f38744420
asif3062 <asifbsb2002@gmail.com>
2022-11-27 21:40:11 +0600

    hotsale section added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Advertise from './Advertise';
 import Banner from './Banner';
+import HotSale from './HotSale';
 
 const home = () => {
     return (
         <div className='p-3 md:p-5'>
             <Advertise></Advertise>
-            
             <Banner></Banner>
+            <HotSale></HotSale>
         </div>
     );
 };
