bff33d69675a71b5ba3061236d6ac21b00afeb22
asif3062 <asifbsb2002@gmail.com>
2022-11-28 22:01:49 +0600

    order saved to db

--- src/pages/home/Product/bookNowModal/BookNowModal.js
+++ src/pages/home/Product/bookNowModal/BookNowModal.js
@@ -4,6 +4,38 @@ import { AuthContext } from '../../../../Auth/AuthProvider';
 const BookNowModal = ({order}) => {
     const {user} = useContext(AuthContext)
     const { _id, name, category, img, resale_price, seller_name, years_of_use, price, post_time,location } = order
+
+    const handleFormSubmit=(e)=>{
+        e.preventDefault()
+        const form =e.target;
+
+        const email = user?.email;
+        const userName = user?.displayName;
+        const itemName = name;
+        const itemPrice = resale_price;
+        const location = form.location.value;
+        const phone = form.phone.value
+
+        const order = {
+            email,
+            userName,
+            itemName,
+            itemPrice,
+            location,
+            phone
+        }
+
+        fetch('http://localhost:5000/orders',{
+            method : "POST",
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(order)
+        })
+
+        console.log(email,userName,itemName,itemPrice,location,phone)
+    }
+
     return (
         <div>
             <>
@@ -12,11 +44,11 @@ const BookNowModal = ({order}) => {
                 <div className="modal-box relative">
                     <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">name</h3>
-                    <form>
+                    <form onSubmit={handleFormSubmit}>
                         <input type="text" placeholder="Name" name='name' defaultValue={user?.displayName} readOnly className="input input-bordered w-full my-1" />
                         <input type="email" placeholder="Email Address" defaultValue={user?.email} readOnly className="input input-bordered w-full my-1" />
                         <input type="text" placeholder="Item name" required name='itemName' readOnly defaultValue={name} className="input input-bordered w-full my-1" />
-                        <input type="text" placeholder="Item Price" required name='itemPrice' readOnly defaultValue={`$ ${resale_price}`} className="input input-bordered w-full my-1" />
+                        <input type="text" placeholder="Item Price" required name='itemPrice' readOnly defaultValue={resale_price} className="input input-bordered w-full my-1" />
                         <input type="text" placeholder="Phone Number" required name='phone' className="input input-bordered w-full my-1" />
                         <input type="text" placeholder="Meeting Location" required name='location' className="input input-bordered w-full my-1" />
                         <br />
