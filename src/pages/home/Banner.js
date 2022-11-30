d1a8e0ea44697f66f516af1102617fd8b2df76b0
asif3062 <asifbsb2002@gmail.com>
2022-11-30 16:17:48 +0600

    banner dummy text changed

--- src/pages/home/Banner.js
+++ src/pages/home/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
             <div className='m-3'>
                 <h1 className='text-3xl font-bold'>Best Place for Your next Smartphone</h1>
                 <p className='text-xl'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus rerum necessitatibus, officiis nihil quaerat amet autem cupiditate a expedita nesciunt vero reiciendis repellendus natus fuga placeat consequatur animi perferendis in quibusdam ad nobis commodi nemo nam eveniet. Quia, harum excepturi!
+                There’s no one smartphone that is best for everyone, but we’ve spent hundreds of hours testing scores of phones so that you can find the best one for you. The phone you buy will depend on your budget, your wireless carrier, and which of the major smartphone platforms—Apple’s iOS or Google’s Android—you’re already invested in.
                 </p>
             </div>
             <img src="https://static1.anpoimages.com/wordpress/wp-content/uploads/2022/03/xiaomi-phones-generic-2.jpg" alt="" className='md:w-1/2 rounded-md' />
